a1ccfb74d81cab8a67e2194acbb870de559163c3
roger <huallanca.roger@gmail.com>
2024-06-26 18:38:39 -0500

    ventana emergente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import Bolivia from "../assets/bolivia.webp";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 function Home() {
@@ -20,25 +21,23 @@ function Home() {
   };
 
   return (
-    <>
-      <Box className=" flex flex-col w-full items-center gap-7">
-        <Typography variant="h3">Campañas</Typography>
-        <a
-          href={`${window.location.origin}/bolivia`}
-          className="relative group"
-          onClick={handleAbrirVentana}
-        >
-          <img
-            className="mask-flag pointer-events-none aspect-[3/2] h-auto w-28 object-contain object-center"
-            src={Bolivia}
-            alt="Bolivia"
-          />
-          <p className="absolute -translate-x-1/2 left-1/2 opacity-0 group-hover:opacity-100 group-hover:-translate-y-2 font-bold uppercase transition-all duration-300">
-            bolivia
-          </p>
-        </a>
-      </Box>
-    </>
+    <Box className="flex flex-col w-full items-center gap-7">
+      <Typography variant="h3">Campañas</Typography>
+      <Link
+        to="/bolivia"
+        className="relative group"
+        onClick={handleAbrirVentana}
+      >
+        <img
+          className="mask-flag pointer-events-none aspect-[3/2] h-auto w-28 object-contain object-center"
+          src={Bolivia}
+          alt="Bolivia"
+        />
+        <p className="absolute -translate-x-1/2 left-1/2 opacity-0 group-hover:opacity-100 group-hover:-translate-y-2 font-bold uppercase transition-all duration-300">
+          bolivia
+        </p>
+      </Link>
+    </Box>
   );
 }
 
