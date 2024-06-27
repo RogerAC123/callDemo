f0f916659b73ede17f84100ba6c563e60e2d39f2
roger <huallanca.roger@gmail.com>
2024-06-26 21:38:34 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Bolivia from "./pages/Bolivia";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/bolivia" element={<Bolivia />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,30 +4,10 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 function Home() {
-  const abrirVentanaEmergente = (url) => {
-    const ancho = 800;
-    const alto = 800;
-    const left = window.innerWidth / 2 - ancho / 2;
-    const top = window.innerHeight / 2 - alto / 2;
-    const opciones = `width=${ancho},height=${alto},left=${left},top=${top}`;
-
-    const fullPath = `${window.location.origin}${url}`;
-    window.open(fullPath, "_blank", opciones);
-  };
-
-  const handleAbrirVentana = (event) => {
-    event.preventDefault();
-    abrirVentanaEmergente("/bolivia");
-  };
-
   return (
     <Box className="flex flex-col w-full items-center gap-7">
       <Typography variant="h3">Campañas</Typography>
-      <Link
-        to="/bolivia"
-        className="relative group"
-        onClick={handleAbrirVentana}
-      >
+      <Link to="/bolivia" className="relative group">
         <img
           className="mask-flag pointer-events-none aspect-[3/2] h-auto w-28 object-contain object-center"
           src={Bolivia}
